51b2a40949d560dd846a486eb80d76109f5e3f5b
nasrin-a-mahin <nasrinamahinofficial@gmail.com>
2026-01-26 13:01:43 +0530

    feat: add student module with role-based access

--- student_management/students/views.py
+++ student_management/students/views.py
@@ -1,35 +1,44 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
 from accounts.decorators import role_required
-
+from django.contrib.auth.decorators import login_required
 @role_required('ADMIN')
 def student_list(request):
     students = Student.objects.all()
     return render(request, 'students/student_list.html', {'students': students})
 
+@login_required
 @role_required('ADMIN')
 def student_create(request):
     if request.method == 'POST':
         Student.objects.create(
-            user_id=request.POST['user'],
             roll_no=request.POST['roll_no'],
-            date_of_birth=request.POST['dob'],
+            name=request.POST['name'],
+            email=request.POST['email'],
             course=request.POST['course'],
-            year=request.POST['year']
         )
         return redirect('student_list')
+
     return render(request, 'students/student_form.html')
 
+
+@login_required
 @role_required('ADMIN')
 def student_update(request, pk):
-    student = get_object_or_404(Student, pk=pk)
+    student = Student.objects.get(pk=pk)
+
     if request.method == 'POST':
         student.roll_no = request.POST['roll_no']
+        student.name = request.POST['name']
+        student.email = request.POST['email']
         student.course = request.POST['course']
-        student.year = request.POST['year']
         student.save()
         return redirect('student_list')
-    return render(request, 'students/student_form.html', {'student': student})
+
+    return render(request, 'students/student_form.html', {
+        'student': student
+    })
+
 
 @role_required('ADMIN')
 def student_delete(request, pk):
