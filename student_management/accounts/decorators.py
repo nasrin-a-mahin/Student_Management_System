fb54b12a6df172d067212a00b46e988d3c84b7a6
nasrin-a-mahin <nasrinamahinofficial@gmail.com>
2026-01-28 16:06:31 +0530

    fix(auth): support multiple roles in role_required decorator

--- student_management/accounts/decorators.py
+++ student_management/accounts/decorators.py
@@ -9,22 +9,26 @@ from functools import wraps
 
 
 
-def role_required(role):
+from django.http import HttpResponseForbidden
+from functools import wraps
+
+def role_required(*roles):
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
 
-            print("Required role:", role)
+            print("Allowed roles:", roles)
             print("User role:", getattr(request.user.profile, 'role', None))
             print("Is superuser:", request.user.is_superuser)
 
+            # Always allow superuser
             if request.user.is_superuser:
                 return view_func(request, *args, **kwargs)
 
-            if request.user.profile.role == role:
+            # Allow if user's role is in allowed roles
+            if request.user.profile.role in roles:
                 return view_func(request, *args, **kwargs)
 
             return HttpResponseForbidden("You are not allowed here")
-
         return _wrapped_view
     return decorator
